4505ad4fe047035a37eefe60b909dfc22ce103a4
Ameena <bibiameena58@gmail.com>
2023-09-22 14:05:58 +0530

    commit JavaProject

---
+++ src/com/staragile/example.java
@@ -0,0 +1,15 @@
+package com.staragile;
+
+public class example {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		short short1= 1;
+		int num1 = 100;
+		float float1=10;
+		double double1=200.20;
+		
+    System.out.println("Welcome to Java");
+	}
+
+}
